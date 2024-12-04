068ebec6718071c07d8aea6c1e57f08f5cc2681d
Igor Faliszewski <74872004+faliszewskii@users.noreply.github.com>
2024-12-04 10:13:09 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# opengl-template
+Template for OpenGL applications
+
+![Zrzut ekranu z 2024-12-02 10-16-58](https://github.com/user-attachments/assets/55e4653c-3294-43f1-bc95-76a86d24753a)
---
+++ src/app/Scene.cpp
@@ -0,0 +1,64 @@
+//
+// Created by faliszewskii on 16.06.24.
+//
+
+#include "Scene.h"
+#include "../interface/camera/CameraAnchor.h"
+
+Scene::Scene(AppContext &appContext) : appContext(appContext) {
+    appContext.camera = std::make_unique<CameraAnchor>(1920, 1080, glm::vec3(0.0f, 3.0f, 3.0f), glm::vec3(0.f), glm::vec3(-M_PI / 4, 0, 0));
+    appContext.frameBufferManager = std::make_unique<FrameBufferManager>();
+    appContext.frameBufferManager->create_buffers(appContext.camera->screenWidth, appContext.camera->screenHeight);
+
+    // TODO --- Initialization of the app state goes here.
+
+    appContext.phongShader = std::make_unique<Shader>(Shader::createTraditionalShader(
+            "../res/shaders/phong/phong.vert", "../res/shaders/phong/phong.frag"));
+    appContext.pointShader = std::make_unique<Shader>(Shader::createTraditionalShader(
+            "../res/shaders/point/point.vert", "../res/shaders/point/point.frag"));
+
+    appContext.quad = std::make_unique<Quad>();
+    appContext.light = std::make_unique<PointLight>();
+    appContext.light->position = {-0.5f , 0.25f, 0.25f};
+    appContext.lightBulb = std::make_unique<Point>();
+
+    appContext.bunny = std::make_unique<Model>("../res/models/stanfordBunny.obj");
+}
+
+void Scene::update() {
+    // TODO --- Here goes scene data update.
+    appContext.lightBulb->position = appContext.light->position;
+    appContext.lightBulb->color = glm::vec4(appContext.light->color, 1);
+}
+
+void Scene::render() {
+    appContext.frameBufferManager->bind();
+
+    auto quadModel = glm::identity<glm::mat4>();
+    quadModel = glm::rotate(quadModel, -float(std::numbers::pi / 2), glm::vec3(1, 0, 0));
+
+    auto bunnyModel = glm::identity<glm::mat4>();
+    bunnyModel = glm::scale(bunnyModel, glm::vec3(2));
+
+    // TODO --- Here goes scene render.
+    appContext.phongShader->use();
+    appContext.phongShader->setUniform("model", quadModel);
+    appContext.phongShader->setUniform("viewPos", appContext.camera->getViewPosition());
+    appContext.phongShader->setUniform("view", appContext.camera->getViewMatrix());
+    appContext.phongShader->setUniform("projection", appContext.camera->getProjectionMatrix());
+    appContext.phongShader->setUniform("material.hasTexture", false);
+    appContext.phongShader->setUniform("material.albedo", glm::vec4(1.0f, 1.0f, 0.0f, 1.0f));
+    appContext.phongShader->setUniform("material.shininess", 256.f);
+    appContext.light->setupPointLight(*appContext.phongShader);
+    appContext.quad->render();
+    appContext.phongShader->setUniform("material.albedo", glm::vec4(1.0f, 0.5f, 0.5f, 1.0f));
+    appContext.phongShader->setUniform("model", bunnyModel);
+    appContext.bunny->render();
+
+    appContext.pointShader->use();
+    appContext.pointShader->setUniform("view", appContext.camera->getViewMatrix());
+    appContext.pointShader->setUniform("projection", appContext.camera->getProjectionMatrix());
+    appContext.lightBulb->render(*appContext.pointShader);
+
+    appContext.frameBufferManager->unbind();
+}
